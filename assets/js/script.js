8cc443b2cd17dd4509084595c316c03a8b153167
Madhusri J <srimadhu.j@gmail.com>
2023-05-18 01:30:29 +0530

    ADD. Sub Content - Completed Application

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,35 @@ function showDiv(divId) {
     var selectedDiv = document.getElementById(divId);
     selectedDiv.classList.remove('hidden');
 }
+
+
+var slideIndex = 0;
+var slides = document.getElementsByClassName('carousel-slide')[0].children;
+var autoplayInterval = 3000; 
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slides[slideIndex].style.display = 'block';
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+function autoplay() {
+    nextSlide();
+}
+
+setInterval(autoplay, autoplayInterval);
+
+showSlide(slideIndex);
