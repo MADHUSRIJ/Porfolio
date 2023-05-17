843152765c9e333c082efe7dc0524bb6b0696bd3
Madhusri J <srimadhu.j@gmail.com>
2023-05-18 00:40:05 +0530

    ADD. Basic Details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-// Initial Script.js //
+function showDiv(divId) {
+    var buttons = document.getElementsByTagName('button');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('active-button');
+    }
+
+    var selectedButton = document.getElementById(divId + 'Button');
+    selectedButton.classList.add('active-button');
+
+    var divs = document.getElementsByClassName('basic-details');
+    for (var j = 0; j < divs.length; j++) {
+        divs[j].classList.add('hidden');
+    }
+    var selectedDiv = document.getElementById(divId);
+    selectedDiv.classList.remove('hidden');
+}
